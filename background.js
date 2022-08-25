2b94d71ec513614a860b5bc7c8c0485e40deaaaf
Anthony Al-Rifai <samo@Anthonys-MacBook-Pro.local>
2022-08-25 13:54:47 -0500

    functionality works, now onto toggle button

--- background.js
+++ background.js
@@ -1,21 +1,40 @@
 //we want an event listener to execute functionality after either a button press or extension loads
 
-//create a function that converts the font of the page to comic sans
-
-function toComicSans() {
-  const divs = document.getElementsByTagName('div');
-  for (const div of divs) {
-    document.body.style.fontFamily = 'Comic Sans MS';
-    divs.style.fontFamily = 'Comic Sans MS';
-  }
-  document.body.style.backgroundColor = 'red';
+// create word scrambler function
+// select all elements of the body, do not include scripts, styles, and noscripts
+//'body *:not(script, noscript, style, header)'
+// 'p, h1, h2, h3, h4, h5, h6, a, td, li'
+function wordScrambler() {
+  document
+    .querySelectorAll('p, h1, h2, h3, h4, h5, h6, a, td, li')
+    .forEach((node) => {
+      const text = node.innerText;
+      if (text) {
+        const words = text.split(' ');
+        words.forEach((word, index) => {
+          if (/^[\.a-zA-Z0-9,!?:'; ]*$/.test(word)) {
+            const letters = word.split('');
+            for (let i = 1; i < letters.length - 1; i += 2) {
+              if (i + 1 !== letters.length - 1) {
+                const current = letters[i];
+                const next = letters[i + 1];
+                letters[i] = next;
+                letters[i + 1] = current;
+              }
+            }
+            words[index] = letters.join('');
+          }
+        });
+        node.innerText = words.join(' ');
+      }
+    });
 }
 
 chrome.action.onClicked.addListener((tab) => {
   if (!tab.url.includes('chrome://')) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: toComicSans,
+      function: wordScrambler,
     });
   }
 });
